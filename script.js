981eaa3d70d880bbf295c7ec05773ec0ec973842
karleene <karleenestanislao@gmail.com>
2022-06-06 19:00:45 -0500

    added hamburger menu

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
+// HAMBURGER MENU
+
+const burgerMenu = document.querySelector('#burgerMenu');
+
+const navList = document.querySelector('#navigation');
 
 
+burgerMenu.addEventListener('click', function () {
+    navList.classList.toggle('active')
+    this.firstElementChild.classList.toggle('fa-bars');
+    this.firstElementChild.classList.toggle('fa-xmark');
+})
